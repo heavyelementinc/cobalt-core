6d1a2d9790674265a0baaced3d8043c58ffb340e
heavyelement <gardiner@heavyelement.io>
2021-04-29 15:09:19 -0400

    Added rebuild subcommand to Project

--- cli/commands/Project.php
+++ cli/commands/Project.php
@@ -7,6 +7,10 @@ class Project{
         'init' => [
             'description' => "Initialized a new project.",
             'context_required' => false
+        ],
+        'rebuild' => [
+            'description' => "Schedule a rebuild of cached settings on next request.",
+            'context_required' => true
         ]
     ];
 
@@ -15,5 +19,12 @@ class Project{
         $this->np->__collect_new_project_settings();
     }
 
+    function rebuild(){
+        $file = __APP_ROOT__ . "/ignored/config/settings.json";
+        if(!file_exists($file)) {
+            if(!mkdir(pathinfo($file,PATHINFO_DIRNAME),true)) throw new Exception("Unable to create APP_ROOT/ignored/config path");
+        }
+        touch($file);
     
+    }
 }
