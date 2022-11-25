5718f6f2f3b9a410a854f67e84e2a4e9d57a9cb5
Gardiner Bryant <gardiner@heavyelemnt.io>
2022-11-25 17:17:47 -0500

    Removed unstable implementation of upgrade process from CLI [project] command

--- cli/commands/Project.php
+++ cli/commands/Project.php
@@ -32,41 +32,6 @@ class Project {
         return "Next web request will regenerate settings cache.";
     }
 
-    // /**
-    //  * Upgrade the git repo to the latest version.
-    //  * 
-    //  * By doing this
-    //  */
-
-    // function upgrade($repo = "all", $force = "") {
-    //     if (!exec("which git")) throw new Exception("You do not have git installed. Aborting.");
-
-    //     $all_repos = ["env" => __ENV_ROOT__, "app" => __APP_ROOT__];
-    //     $repos = $all_repos;
-
-    //     if ($repo !== "all" && !key_exists($repo, $all_repos)) throw new Exception("Invalid option");
-    //     else if ($repo !== "all") $repos = [$all_repos[$repo]];
-
-    //     $git_pull = "git pull";
-    //     if ($force === "force") $git_pull = "git reset --hard";
-
-    //     $results = [];
-    //     $errors = [];
-    //     foreach ($repos as $name => $dir) {
-    //         chdir($dir);
-    //         $result = exec("git status | echo $?");
-    //         if (!ctype_digit($result)) continue; // The DIR isn't a repo, skip it
-    //         if ((int)$result < 0 || (int)$result > 128)  throw new Exception("Unexpected git repo status. Aborting");
-
-    //         $results[$name] = exec("$git_pull | echo $?");
-    //         if (!ctype_digit($results[$name])) $errors[$name] = $results[$name];
-    //     }
-
-    //     if (!empty($errors[$name])) throw new Exception(implode("\n", $errors) . "\n" . fmt("Errors occurred. Try cobalt project <app> force"));
-    //     if (count($results) === 2) return "Projects updated";
-    //     return "Environment updated.";
-    // }
-
     function get_deps() {
     }
 }
