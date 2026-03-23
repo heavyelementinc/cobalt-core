f6af68ed1c582d696414187eac07143104ae3a69
Gardiner Bryant <gardiner@heavyelement.io>
2026-03-23 04:33:25 +0000

    Fixed an issue where the post_create method was not called after an object was created

--- Cobalt/Controllers/Traits/CreateableModel.php
+++ Cobalt/Controllers/Traits/CreateableModel.php
@@ -65,6 +65,7 @@ trait CreateableModel {
         // Now, let's insert our content into the database.
         $result = $schema->insertOne($mutant);
         $insertedId = $result->getInsertedId();
+        $this->post_create($schema, $insertedId, $result);
         if(method_exists($this, "postCreate")) $this->postCreate($result, $insertedId, $result);
         // Let's check if we need to grab a route and redirect (if this item is updatable)
         $route = route("$this->name@__edit", [(string)$insertedId]);
